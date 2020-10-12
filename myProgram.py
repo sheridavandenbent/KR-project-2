1474837d2587cf80299056361bdc3d07776e5145
Sherida van den Bent <sheridavandenbent@gmail.com>
2020-10-12 21:41:22 +0200

    Loop for order of forgetting

--- myProgram.py
+++ myProgram.py
@@ -1,49 +1,59 @@
 import os
 
+inputOntology = "datasets/pizza.owl"
+inputSubclassStatements = "datasets/subClasses.nt"
+forgetOntology = "datasets/pizza.owl"
+method = "3"
+signature = "datasets/signature.txt"
+
 
 def CreateForgetOrder(forget_input):
     forget_file = open(forget_input, "r")
 
     forget_order = []
     for line in forget_file:
-        forget_order.append()
+        forget_order.append(line)
     return forget_order
 
 def main():
-    forget_file = ''
+    global forgetOntology
+    forget_file = "./forget_this.txt"
     forget_order = CreateForgetOrder(forget_file)
 
-    signature_file = open(signature, 'rw')
-
+    
     for concept in forget_order:
-        '''
-        put concept into signature_file
-        run lethe command
-        update ForgetOntology with result.owl
-        '''
+        with open(signature, 'w') as f: # overwrites file every time
+            f.write(concept)
+
+        os.system(
+            'java -cp lethe-standalone.jar uk.ac.man.cs.lethe.internal.application.ForgettingConsoleApplication --owlFile '
+            + forgetOntology + ' --method ' + method + ' --signature ' + signature)
+
+        forgetOntology = './result.owl'
 
 # This is an example puython programme which shows how to use the different stand-alone versions of OWL reasoners and forgetting programme
 
-# Choose the ontology (in the OWL format) for which you want to explain the entailed subsumption relations.
-inputOntology = "datasets/pizza.owl"
+# # Choose the ontology (in the OWL format) for which you want to explain the entailed subsumption relations.
+# inputOntology = "datasets/pizza.owl"
 
-# Choose the set of subclass for which you want to find an explanation.
-# this file can be generated using the second command (saveAllSubClasses)
-inputSubclassStatements = "datasets/subClasses.nt"
+# # Choose the set of subclass for which you want to find an explanation.
+# # this file can be generated using the second command (saveAllSubClasses)
+# inputSubclassStatements = "datasets/subClasses.nt"
 
-# Choose the ontology to which you want to apply forgetting. This can be the inputOntology, but in practise
-# should be a smaller ontology, e.g. created as a justification for a subsumption
-forgetOntology = "datasets/pizza.owl"
+# # Choose the ontology to which you want to apply forgetting. This can be the inputOntology, but in practise
+# # should be a smaller ontology, e.g. created as a justification for a subsumption
+# forgetOntology = "datasets/pizza.owl"
 
-# Decide on a method for the forgetter (check the papers of LETHE to understand the different options).
-# The default is 1, I believe.
-# 1 - ALCHTBoxForgetter
-# 2 - SHQTBoxForgetter
-# 3 - ALCOntologyForgetter
-method = "3"  #
+# # Decide on a method for the forgetter (check the papers of LETHE to understand the different options).
+# # The default is 1, I believe.
+# # 1 - ALCHTBoxForgetter
+# # 2 - SHQTBoxForgetter
+# # 3 - ALCOntologyForgetter
+# method = "3"  #
 
-# Choose the symbols which you want to forget.
-signature = "datasets/signature.txt"
+# # Choose the symbols which you want to forget.
+# signature = "datasets/signature.txt"
+
 
 # 1. PRINT ALL SUBCLASSES (inputOntology):
 # print all subClass statements (explicit and inferred) in the inputOntology
@@ -68,5 +78,7 @@ signature = "datasets/signature.txt"
 
 # For running LETHE forget command:
 # --> uncomment the following line to run this function
-os.system(
-    'java -cp lethe-standalone.jar uk.ac.man.cs.lethe.internal.application.ForgettingConsoleApplication --owlFile ' + forgetOntology + ' --method ' + method + ' --signature ' + signature)
+# os.system(
+#     'java -cp lethe-standalone.jar uk.ac.man.cs.lethe.internal.application.ForgettingConsoleApplication --owlFile ' + forgetOntology + ' --method ' + method + ' --signature ' + signature)
+
+main()
